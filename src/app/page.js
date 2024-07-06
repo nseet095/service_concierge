0f6d83192561ecbbcdfd9d6de76ed3b4e53a2f36
nseet095 <114053998+nseet095@users.noreply.github.com>
2024-07-06 19:36:11 +0800

    Fixed carousel not showing
    Fixed css when displaying carousel on small screen size devices

--- src/app/page.js
+++ src/app/page.js
@@ -7,21 +7,32 @@ const Home = () => {
   return (
     <>
       <TopBar active="home" />
-      <div className="relative h-2/3 sm:h-64 xl:h-80 2xl:h-96 min-h-8">
-        <Carousel
-          pauseOnHover
-          slideInterval={3000}
-          className=""
-        >
-        {/* TODO: Change to use nextjs Image element */}
-          <img className="blur-sm hover:blur-none" src="/carousel/1.jpg" alt="placeholder 1" />
-          <img className="blur-sm hover:blur-none" src="/carousel/2.jpg" alt="placeholder 2" />
-        </Carousel>
+      <div className="relative text-center">
+        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+          <Carousel pauseOnHover slideInterval={3000} className="">
+            {/* TODO: Change to use nextjs Image element */}
+            <Image
+              className="blur-sm hover:blur-none"
+              src="/carousel/1.jpg"
+              alt="placeholder 1"
+              width={800}
+              height={800}
+            />
+            <Image
+              className="blur-sm hover:blur-none"
+              src="/carousel/2.jpg"
+              alt="placeholder 2"
+              width={800}
+              height={800}
+            />
+          </Carousel>
+        </div>
         <Title title="Service Concierge" subtitle="Some witty quote" />
       </div>
       <article className="flex flex-col justify-center items-center text-center bg-inherit">
-        <h2 className="text-4xl font-extrabold text-black dark:text-white">OUR SERVICES</h2>
-       
+        <h2 className="text-4xl font-extrabold text-black dark:text-white">
+          OUR SERVICES
+        </h2>
       </article>
     </>
   );
